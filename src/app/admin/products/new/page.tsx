d7bec09dd3b0cbb156714771c58a3ed912444cf9
Bart Dmochowski <bartoszdmochowski2@gmail.com>
2025-12-25 10:53:07 +0100

    Dodanie dynamic do wszystkich pozostałych stron z prisma

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -1,4 +1,7 @@
 import { prisma } from '@/lib/prisma'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import NewProductForm from './NewProductForm'
 
 export default async function NewProductPage() {
